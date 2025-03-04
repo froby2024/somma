f2c1794e3b2f63bb646923c49b69008a87fe1342
froby2024 <167176248+froby2024@users.noreply.github.com>
2025-03-04 18:42:14 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
+# Importazione del modulo Python Flask
 from flask import Flask
 
+
+# Il costruttore di Flask utilizza il nome del modulo corrente
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-   return "Hello, Docker!"
 
+# La funzione route() è un decorator,
+# Lega un URL a una funzione associata
+@app.route('/')
+def hello_world():
+return 'Hello World'
+
+
+# Se il file viene eseguito direttamente
+# Eseguire l'applicazione Flask
 if __name__ == '__main__':
-   app.run(host='0.0.0.0')
+app.run(debug=True, host='0.0.0.0')
